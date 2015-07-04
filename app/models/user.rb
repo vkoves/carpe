4e45778957b3676bba9570dbc7326733308a32de
Viktor Koves <viktorwdgk25@gmail.com>
2015-07-04 16:54:51 -0500

    Made name be required on sign up and also be editable in edit profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
 	devise :omniauthable
+	validates_presence_of :name
 	
 	def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
