be9f7dbe7b1febeb6516e371f6f93a406853a9b5
Robert J. Altman <rjaltman04@gmail.com>
2015-08-08 02:37:27 -0500

    Unfinished dynamic categories with CRUD

--- db/migrate/20150807060454_create_categories.rb
+++ db/migrate/20150807060454_create_categories.rb
@@ -3,6 +3,7 @@ class CreateCategories < ActiveRecord::Migration
     create_table :categories do |t|
       t.string :name
       t.string :color
+      t.string :user
 
       t.timestamps null: false
     end
