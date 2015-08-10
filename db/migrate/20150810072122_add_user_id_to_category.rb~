01021c27847100a44804d29614b4aa7b84ca1717
Robert J. Altman <rjaltman04@gmail.com>
2015-08-10 02:52:30 -0500

    Added user has_many Category association

---
+++ db/migrate/20150810072122_add_user_id_to_category.rb~
@@ -0,0 +1,6 @@
+class AddUserIdToCategory < ActiveRecord::Migration
+  def change
+    add_column :categories, :user_id, :integer
+    add_index :categories, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807060454) do
+ActiveRecord::Schema.define(version: 20150810072122) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20150807060454) do
     t.string   "user"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "categories", ["user_id"], name: "index_categories_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
