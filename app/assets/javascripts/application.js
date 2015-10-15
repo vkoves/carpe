73f2356cad508cb28e530ec73b4f967b0d349714
Viktor Koves <viktorkoves@gmail.com>
2015-10-15 01:01:29 -0500

    Made the user panel a slide toggle and added hover effects to each of the links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,8 +20,7 @@
 
 $(document).ready(function()
 {
-	
-$("#user-name-panel").click(function(){
-	$("#user-panel").toggle();
-});
+	$("#user-name-panel").click(function(){
+		$("#user-panel").slideToggle();
+	});
 });
