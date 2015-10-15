f799e3e937e88d6ad0f37ddeebb0550ed4407b7d
Viktor Koves <viktorkoves@gmail.com>
2015-10-14 23:54:53 -0500

    Removed profile page and manipulated show to have a large icon of the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,5 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find_by_id(params[:id])
-    
-    if @user = current_user
-      render "profile" #render a different page if this is the current user
-    end
   end
 end
