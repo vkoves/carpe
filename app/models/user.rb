df71ed7a4bec3d53a3f71403716c504b1c58f955
Viktor Koves <viktorkoves@gmail.com>
2015-10-14 22:02:18 -0500

    Added user_avatar method, which returns the google avatar of the requested size, or the default avatar if one does not exist. Also increased the size of displayed avatars for easier viewing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,14 @@ class User < ActiveRecord::Base
 
 	has_many :categories
 	
+	def user_avatar(size) #returns a url to the avatar with the width in pixels
+	 if image_url
+	   return image_url.split("?")[0] + "sz=" + size.to_s
+	 else
+	   return "http://www.gravatar.com/avatar/?d=mm"
+	 end
+	end
+	
 	def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
     user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
