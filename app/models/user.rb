4bba35fa6a7dada94f7cc5ef5e9f3533c2aceeea
Viktor Koves <viktorkoves@gmail.com>
2015-10-17 00:54:36 -0500

    Fixed friends being readdable if they are an inverse friend, moved more logic into user model, and added friend button and label on profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,6 +37,18 @@ class User < ActiveRecord::Base
     return all_friends 
 	end
 	
+	def is_friend?(user)
+	  if friends.include? user or inverse_friends.include? user
+	    return true
+	  else
+	    return false
+	  end
+	end
+	
+	def number_of_friends()
+	  return friendships.count + inverse_friends.count
+	end
+	
 	def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
     user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
