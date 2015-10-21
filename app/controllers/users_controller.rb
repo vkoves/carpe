36e58713fc9ccfbc5924dbbf6c9ddd1230a184b9
Viktor Koves <viktorkoves@gmail.com>
2015-10-20 22:02:24 -0500

    Fixed errors from not being logged in caused by displaying mutual friends and add friend buttons on profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_id(params[:id]) #fetch the user by the URL passed id
     @all_friends = @user.all_friendships #and fetch all of the user's friends
+    @profile = false
+    if current_user and current_user == @user #this is the user looking at their own profile
+      @profile = true
+    end
   end
 end
