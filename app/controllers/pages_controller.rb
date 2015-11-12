f40ea94354f22175d8825d2ea1093ece9fef5a5c
Viktor Koves <viktorkoves@gmail.com>
2015-11-12 01:30:29 -0600

    Moved ruby code from view to controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,34 @@
 class PagesController < ApplicationController
   def schedule
-    
+    if current_user
+      if params[:new] == "t"
+        newCat = Category.new
+        newCat.user = current_user #set the user to the logged in user
+        newCat.color = params[:col]
+        newCat.name = params[:name]
+        newCat.save
+        redirect_to "/schedule"
+      elsif params[:dest] == "t"
+        id = params[:id]
+        Category.find_by_id(id).destroy
+        redirect_to "/schedule"
+      elsif params[:edit] == "t"
+        id = params[:id]
+        name = params[:name]
+        col = params[:col]
+        findCat = Category.find_by_id(id)
+        findCat.name = name
+        findCat.color = col
+        findCat.save  
+        redirect_to "/schedule"
+      elsif params[:clearAll] == "t"
+        Category.all.each_with_index do |cat, index|
+          cat.destroy
+        end
+        redirect_to "/schedule"
+      end 
+    else 
+      redirect_to user_session_path;
+    end
   end
 end
