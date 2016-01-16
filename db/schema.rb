29f7b406b79e957df842906d30bcd22ead53aadd
Viktor Koves <viktorkoves@gmail.com>
2016-01-16 02:20:01 -0600

    Added admin column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114061654) do
+ActiveRecord::Schema.define(version: 20160116081934) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20160114061654) do
     t.string   "uid"
     t.string   "name"
     t.string   "image_url"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
