1879ba4c03671c2cf68cc5bd62cb56e574fa4ffe
Viktor Koves <viktorkoves@gmail.com>
2016-01-20 01:34:58 -0600

    Added some code that checks for valid images, but has issues dealing with a valid website that is not an image, such as an imgur gallery. Fix and then re-enable

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,25 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # Overwrite update_resource to let users to update their user without giving their password
   def update_resource(resource, params)
+    if params[:image_url] and false
+      begin
+        url = URI.parse(params[:image_url])
+        Net::HTTP.start(url.host, url.port, :use_ssl => true) do |http|
+          if(!http.head(url.request_uri)['Content-Type'].include? 'image')
+            flash[:notice] = nil
+            flash[:alert] = "That image isn't valid, please check your URL!"
+            params[:image_url] = current_user.image_url
+            #return
+          end
+        end
+      rescue URI::InvalidURIError, Exception => e
+          flash[:alert] = "That image isn't valid, please check your URL!"
+          flash[:notice] = nil
+          params[:image_url] = current_user.image_url
+          #return
+      end
+    end
+    
     if current_user.provider
       params.delete("current_password")
       resource.update_without_password(params)
