1502adacdadb2f1ff37eafe607ce32a396a590d4
Viktor Koves <viktorkoves@gmail.com>
2016-01-21 02:15:04 -0600

    Added privacy checking to event display, which simply does not show the category or events if you do not have proper access

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,4 +6,19 @@ class Category < ActiveRecord::Base
     events.destroy_all
     self.delete
 	end
+	
+	#returns whether the current user can see this
+	def has_access(user_in)
+	  if privacy == "public"
+	    return true
+	  elsif privacy == "private"
+	    return false
+	  elsif privacy == "friends"
+	    if user_in and user_in.friend_status(user) == "friend"
+	      return true
+	    else
+	      return false
+	    end
+	  end
+	end
 end
