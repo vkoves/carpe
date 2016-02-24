e6b7f4233c20fbcebb81c3555d0b58ac0d4f18ec
Viktor Koves <viktorkoves@gmail.com>
2016-02-23 23:29:56 -0600

    Made friends sidebar hidden by default and slide out on mobile. Still needs styling improvements badly.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,25 +22,49 @@
 // require_tree .
 
 
-var ready = function() {
+var ready = function()
+{
 	console.log("Ready " + window.location.href );
-	$("#user-name-panel").click(function(){
+	$("#user-name-panel").click(function()
+	{
 		$("#user-panel").slideToggle(300);
 		$("#notif-panel").slideUp(300);
 	});
-	$(".bell-hold").click(function(){
+	$(".bell-hold").click(function()
+	{
 		$("#notif-panel").slideToggle(300);
 		$("#user-panel").slideUp(300);
 	});
-	$("#header-mob-menu").click(function(){
+	$("#header-mob-menu").click(function()
+	{
 		$("#mobile-menu").slideToggle(300);
+	});
+	$("#sidebar-button").click(function()
+	{
+		if(parseInt($("#sidebar").css("right")) < 0)		{
+			$("#sidebar-button").css("right", "340px");
+			$("#sidebar").css("right", "0%");
+		}
+		else
+		{
+			$("#sidebar-button").css("right", "0%");
+			$("#sidebar").css("right", "-340px");
+		}
 	});
 };
 
 $(window).resize(function()
 {
 	if($( window ).width() > 660)
+	{
 		$("#mobile-menu").slideUp(300);
+		$("#sidebar").css("right", "0%");
+	}
+	else
+	{
+		$("#sidebar").css("right", "-340px");
+		$("#sidebar-button").css("right", "0%");
+	}
 });
 
 $(document).ready(ready);
