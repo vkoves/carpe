2f71c4e70c3b0175027550a45d56ce364b1dfd87
Viktor Koves <viktorkoves@gmail.com>
2016-05-31 23:53:50 -0500

    SEARCH - Fixed ranks overwriting if a search query started off a users last and first name. For instance, S should give Sam Samson a rank of 2, but gave a rank of 1 because of overwrite.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,11 +48,19 @@ class UsersController < ApplicationController
           a_rank = 0
           b_rank = 0
 
-          a_rank = 2 if a.name.downcase.starts_with?(q.downcase) #if the users name starts with the query, it's a great match
-          b_rank = 2 if b.name.downcase.starts_with?(q.downcase)
-
-          a_rank = 1 if a.name.downcase.include?(" " + q.downcase) #if the users middle or last name start with the query, it's an okay match
-          b_rank = 1 if b.name.downcase.include?(" " + q.downcase)
+          
+          if a.name.downcase.starts_with?(q.downcase) #if the users name starts with the query
+            a_rank = 2 #it's a great match (score 2)
+          elsif a.name.downcase.include?(" " + q.downcase) #if the users middle or last name starts with the query
+            a_rank = 1 #it's an okay match (score 1)
+          end #otherwise we get the default score of 0 for having the query in their name
+            
+          #repeat for b
+          if b.name.downcase.starts_with?(q.downcase)
+            b_rank = 2
+          elsif b.name.downcase.include?(" " + q.downcase)
+            b_rank = 1
+          end
 
           b_rank <=> a_rank #return comparison of ranks, with highest preferred first
         } 
