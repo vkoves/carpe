ab3b0c7eda661ade86e4f6ee5961bdf6de63896d
Viktor Koves <viktorkoves@gmail.com>
2016-05-31 23:23:38 -0500

    EMAIL - Updated email subject line and tried styling email.

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -4,7 +4,6 @@ class UserNotifier < ApplicationMailer
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
     @user = user
-    mail( :to => @user.email,
-    :subject => 'Thanks for signing up for our amazing app' )
+    mail(:to => @user.email, :subject => 'Welcome to Carpe')
   end
 end
