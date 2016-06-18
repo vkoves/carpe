7cc85c1115cf95e11ad8cc2ff4a4300465b33b9f
Viktor Koves <viktorkoves@gmail.com>
2016-06-18 15:57:54 -0500

    TESTING, TDD - Wrote improved tests for event, testing whether the repeat_events_in_range results have proper dates

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -4,14 +4,41 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
   test "repeat start and end shouldn't impact original event if repeat type is none" do
-  	start_date_time = DateTime.parse("2015-12-14 00:00:00")
-  	end_date_time = DateTime.parse("2015-12-17 00:00:00")
-  	repeat_dates = events(:repeat_none_with_start_and_end).events_in_range(start_date_time, end_date_time)
-  	assert repeat_dates.length == 1, "Repeat dates length was supposed to be 1, was " + repeat_dates.length.to_s
+  	event = events(:repeat_none_with_start_and_end)
+  	start_date_time = (event.date - 2.day).to_datetime
+  	end_date_time = (event.end_date + 2.day).to_datetime
+  	repeat_events = event.events_in_range(start_date_time, end_date_time)
+  	assert repeat_events.length == 1, "Repeat dates length was supposed to be 1, was " + repeat_events.length.to_s
+  end
+
+  test "repeat daily should happen once a day" do
+  	event = events(:repeat_daily)
+  	#Create a 4 day date range
+  	start_date_time = (event.date - 2.day).to_datetime
+  	end_date_time = (event.end_date + 2.day).to_datetime
+  	#Get all the repeat instances
+  	repeat_events = event.events_in_range(start_date_time, end_date_time)
+  	#And make sure there were 4 of them, since this repeats daily
+  	assert repeat_events.length == 4, "Repeat dates length was supposed to be 4, was " + repeat_events.length.to_s
+  end
+
+  test "repeat clones should have proper date" do
+    event = events(:repeat_daily)
+    #Create a 4 day date range
+    start_date_time = (event.date - 2.day).to_datetime
+    end_date_time = (event.end_date + 2.day).to_datetime
+    repeat_dates = (start_date_time ... end_date_time).to_a #create a range of dates and convert to array
+    repeat_dates = repeat_dates.map{|d| d.to_date} #convert to date from date_time
+
+    #Get all the repeat instances
+    repeat_events = event.events_in_range(start_date_time, end_date_time)
+    #Then get the dates the repeat instances fall on
+    repeat_events_dates =  repeat_events.map{|r| r.date.to_date}
+
+    #Then test that the repeat instance dates are the same as the range of dates, since this event repeats daily
+    assert repeat_dates - repeat_events_dates == [], "The repeat clones dates were supposed to line up with the date range." + 
+    "\nRepeat clones dates were instead \n" + repeat_events_dates.to_s +
+    "\nWhile the date range was \n" + repeat_dates.to_s
   end
 end
