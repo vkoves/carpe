11715d7445eb8e5310f6fd4b2990313706ac3963
Viktor Koves <viktorkoves@gmail.com>
2016-06-18 13:04:15 -0500

    FIX, TDD - Fixed including Devise test helpers in all tests, and included them specifically in just controller tests, as they broke the event test.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   test "should not get admin if not signed in" do
     get :admin
     assert_response :redirect
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,5 +7,4 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
-  include Devise::TestHelpers #include devise to allow tests to work with user stuff
 end
