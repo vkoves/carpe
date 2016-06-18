c5a5ba9ef9fd3607b2f9bf2bbf56d1f6a9edf758
Viktor Koves <viktorkoves@gmail.com>
2016-06-18 12:56:43 -0500

    TESTING, TDD - Wrote real pages controller test for admin page and included the devise helper for tests to fix issues with calling current_user in controllers.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  test "should get schedule" do
-    get :schedule
-    assert_response :success
+  test "should not get admin if not signed in" do
+    get :admin
+    assert_response :redirect
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,5 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include Devise::TestHelpers #include devise to allow tests to work with user stuff
 end
