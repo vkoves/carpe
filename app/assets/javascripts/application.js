a8453f6d192306dc1cde908993461b1733e6c03a
Viktor Koves <viktorkoves@gmail.com>
2016-06-26 19:32:16 -0500

    FIX, SANDBOX - Made tokenInput prevent duplicates using custom onAdd functionality, as default duplicate handling resulted in strange issues of where the input was placed (it ended up between names/tokens)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,6 +25,8 @@
 // require_tree .
 
 
+var unloadAssigned = false; //if unload is assigned
+
 //Handle window resizing
 $(window).resize(function()
 {
@@ -204,6 +206,24 @@ function initializeEventListeners()
 		placeholder: "Add people",
 		searchDelay: 0,
 		animateDropdown: false,
+		onAdd: function(item)
+		{
+			var field = $(this).closest("form").find("> input[type=text]");
+			var usersSelected = field.tokenInput("get");
+	        
+	        var itemCount = 0; //how many times this item occurs
+	        for(var i = 0; i < usersSelected.length; i++)
+	        {
+	        	if(usersSelected[i].id == item.id) //if this is the item
+	        		itemCount++; //increment
+	        }
+
+	        if(itemCount > 1) //if this is a duplicate
+	        {
+	        	field.tokenInput("remove", {id: item.id}); //remove all copies
+	        	field.tokenInput("add", item); //and add it back
+	        }
+		},
 		resultsFormatter: function(element)
 		{
 			img_url = element.image_url || "http://www.gravatar.com/avatar/?d=mm";
