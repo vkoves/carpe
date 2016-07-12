018c5a0138f275d41f879febec516d38739a0d95
Viktor Koves <viktorkoves@gmail.com>
2016-07-11 22:43:50 -0500

    TESTS, TDD - Added two basic category tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,4 +4,16 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "not signed in users don't have permission to see private categories" do
+  	assert categories(:private).has_access?(nil) == false, "Not signed in user could access the private category!"
+  end
+
+  test "not signed in users should have permission to see private categories" do
+  	assert categories(:public).has_access?(nil) == true, "Not signed in user couldn't access the public category!"
+  end
+
+  # test "friend user should have permission to see friends categories" do
+  	# assert categories(:friend).has_access(users(:viktors_friend)) == true, "Friend doesn't have access to friends category!"
+  # end
 end
