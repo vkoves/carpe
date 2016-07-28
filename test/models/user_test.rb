19b90e3e27705b9203ae8f64d76be8feb08660a4
Viktor Koves <viktorkoves@gmail.com>
2016-07-27 22:34:15 -0500

    TEST, TDD - Added basic test to verify following functionality is all checked out

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
+# To run all tests, in the project directory run the command:
+# bundle exec rake test
+# ----------------------------------------
+# To run this test, in the project directory run the command:
+# bundle exec rake test test/controllers/event_test.rb
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should follow and unfollow a user" do
+    norm = users(:norm)
+    viktor  = users(:viktor)
+    assert_not norm.following?(viktor)
+    norm.follow(viktor)
+    assert norm.following?(viktor)
+    norm.unfollow(viktor)
+    assert_not norm.following?(viktor)
+  end
 end
