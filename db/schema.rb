00838fc53680e4c908442318165c34746a00d8c8
Viktor Koves <viktorkoves@gmail.com>
2016-08-06 18:40:13 -0500

    DB, MIGRATE - Added public field to users, which determines if the user wants to automatically accept followers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727051646) do
+ActiveRecord::Schema.define(version: 20160806233719) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 20160727051646) do
     t.string   "name"
     t.string   "image_url"
     t.boolean  "admin"
+    t.boolean  "public"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
