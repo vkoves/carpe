e6b6af267487e1385e91fccc40a5706b625b59b9
Viktor Koves <viktorkoves@gmail.com>
2016-10-25 14:17:39 -0500

    Fixed critical error with .includes being run on an empty array Fixed get_events_attributes and get_category_attributes running on an empty array, which caused a 500 error on lots of pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -139,24 +139,34 @@ module ApplicationHelper
   # Get attributes for events, particularly pulling in break_ids
   def get_event_attributes(events)
     eventAttributes = []
-    events.includes(:repeat_exceptions).each do |event|
-      if @group or (@user and !event.group) # don't show group events on a user's schedule, even if they made it?
-        atr = event.attributes
-        atr[:break_ids] = event.repeat_exception_ids #.repeat_exceptions.pluck(:id)
-        eventAttributes.push(atr)
+    
+    if !events.empty? # don't try making extra queries on an empty set
+      events.includes(:repeat_exceptions).each do |event|
+        if @group or (@user and !event.group) # don't show group events on a user's schedule, even if they made it?
+          atr = event.attributes
+          atr[:break_ids] = event.repeat_exception_ids #.repeat_exceptions.pluck(:id)
+          eventAttributes.push(atr)
+        end
       end
+      return eventAttributes
+    else
+      return nil
     end
-    return eventAttributes
   end
 
   # Get attributes for categories, particularly pulling in break_ids
   def get_category_attributes(categories)
     categoryAttributes = []
-    categories.includes(:repeat_exceptions).each do |category|
-      atr = category.attributes
-      atr[:break_ids] = category.repeat_exception_ids
-      categoryAttributes.push(atr)
+
+    if !categories.empty? # don't try making extra queries on an empty set
+      categories.includes(:repeat_exceptions).each do |category|
+        atr = category.attributes
+        atr[:break_ids] = category.repeat_exception_ids
+        categoryAttributes.push(atr)
+      end
+      return categoryAttributes
+    else
+      return nil
     end
-    return categoryAttributes
   end 
 end
