a5cd035b0b113edd0be3fb26da51ba782d93f17a
Viktor Koves <viktorkoves@gmail.com>
2016-12-26 23:40:24 -0600

    Added Home Time Zone dropdown for users The Home Time Zone is now a field users need to fill out when signing up and can edit at any time afterwards through the normal edit profile option. It is in dropdown format of all available time zones using the rails time_zone_select helper

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -34,10 +34,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image_url)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image_url, :home_time_zone)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :email, :public_profile, :password, :password_confirmation, :current_password, :image_url)
+    params.require(:user).permit(:name, :email, :public_profile, :password, :password_confirmation, :current_password, :image_url, :home_time_zone)
   end
 end
