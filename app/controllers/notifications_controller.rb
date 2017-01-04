88faef3d98a5e10b22c7b007de5d656de902a115
Viktor Koves <viktorkoves@gmail.com>
2017-01-04 01:56:21 -0600

    Fixed not being able to mark all notifications as read due to lack of template

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,5 +2,6 @@ class NotificationsController < ApplicationController
   def read_all #Mark all current notifications as read
     @notifications = current_user.notifications
     @notifications.update_all viewed: true
+    render :text => "" # Render blank text
   end
 end
