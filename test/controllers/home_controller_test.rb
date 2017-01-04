5c95f78f6cc752491d16d415b03ff636ca702abb
Viktor Koves <viktorkoves@gmail.com>
2017-01-04 02:13:51 -0600

    Added tests that specifically check for welcome page and dashboard These actually check the tempalte rendered to make sure you get the right template depending on whether you are signed in or not

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,11 +1,18 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  include Devise::TestHelpers  
+  include Devise::TestHelpers
 
-  test "should get index" do
+  test "should get homepage when not signed in" do
     get :index
+    assert_template("home/home", "home should be rendered if not signed in")
     assert_response :success
   end
 
+  test "should get dashboard when signed in" do
+    sign_in users(:viktor)
+    get :index
+    assert_template("home/dashboard", "dashboard should be rendered if signed in")
+    assert_response :success
+  end
 end
