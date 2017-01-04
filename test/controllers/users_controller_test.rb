d2a9100996695eb14c6eddd57ad7057dd948c16d
Viktor Koves <viktorkoves@gmail.com>
2017-01-04 01:12:42 -0600

    Added basic user profile tests Added test checking that signed in and not signed in users can view a profile. Currently per issue #62 user profiles cannot be viewed when not logged in

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,7 +7,20 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   test "should route to user" do #test if the users route is working properly
     assert_routing '/u/1', { controller: "users", action: "show", id: "1" }
+  end
+
+  test "signed in user should be able to view other user" do
+  	sign_in users(:viktor)
+  	get :show, id: users(:norm).id
+  	assert_response :success
+  end
+
+  test "non-signed in user should be able to view user" do
+  	get :show, id: users(:norm).id
+  	assert_response :success
   end
 end
