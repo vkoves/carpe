e1298fd92bcb287c1224e85984eee6e2afe7551a
Robert Altman <rjaltman04@gmail.com>
2017-01-05 22:18:05 -0600

    Email: Lightened gray a bit, and also adjusted margins for footer and rest of table

--- test/mailers/previews/user_notifier_preview.rb
+++ test/mailers/previews/user_notifier_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_notifier
 class UserNotifierPreview < ActionMailer::Preview
 	def send_signup_email
-		UserNotifier.send_signup_email(User.find(9))
+		UserNotifier.send_signup_email(User.find(10))
 	end
 end
