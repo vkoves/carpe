fb39a42c8e3ba5cda4f8cc8c0fcb403abcb9e155
Viktor Koves <viktorkoves@gmail.com>
2017-03-19 23:42:19 -0500

    Added test to verify being able to make a user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -19,10 +19,18 @@ class UserTest < ActiveSupport::TestCase
 
     norm.follow(viktor) # Have Norm follow Viktor
     viktor.confirm_follow(norm) # and Viktor confirm the follow
-    
+
     assert norm.following?(viktor), "Following a user does not work, at least by \".following?\"" # after that, Norm should be following Viktor
-    
+
     norm.unfollow(viktor) # Then have Norm unfollow Viktor
     assert_not norm.following?(viktor), "Unfollowing a user does not work, at least by \".following?\"" # and check it worked
+  end
+
+  test "should be able to make a user with name, email, and password" do
+    test_user = User.new
+    test_user.name = "test"
+    test_user.email = "test@email.com"
+    test_user.password = "password"
+    assert_equal(test_user.save!, true, "Can save a user with only name")
   end
 end
