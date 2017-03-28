0dfccc950dff69e109b81e98f738d8460800157b
Krisztián Köves <kkoves3@gmail.com>
2017-03-28 23:44:16 +0200

    Added test for current_events function of User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,16 +13,49 @@ class UserTest < ActiveSupport::TestCase
 
   test "should follow and unfollow a user" do
     norm = users(:norm) # Grab our two users
-    viktor  = users(:viktor)
+    viktor = users(:viktor)
 
     assert_not norm.following?(viktor), "Users should not be following each other by default." # By default, Norm should not be following Viktor
 
     norm.follow(viktor) # Have Norm follow Viktor
     viktor.confirm_follow(norm) # and Viktor confirm the follow
-    
+
     assert norm.following?(viktor), "Following a user does not work, at least by \".following?\"" # after that, Norm should be following Viktor
-    
+
     norm.unfollow(viktor) # Then have Norm unfollow Viktor
     assert_not norm.following?(viktor), "Unfollowing a user does not work, at least by \".following?\"" # and check it worked
+  end
+
+  test "current_events should return events in progress" do
+    viktor = users(:viktor)
+    curr_event_1 = events(:current_event_1)
+    curr_event_2 = events(:current_event_2)
+
+    # First event that we make into a current event; this one should be first in
+    # the current events array, since it ends in one hour from the current time
+    # (the time when the test is run), and the list is sorted by ending time
+    curr_event_1.date = DateTime.now - 1.hour
+    curr_event_1.end_date = DateTime.now + 1.hour
+    curr_event_1.save
+
+    # This will be second in the current events array, since it ends in two hours
+    curr_event_2.date = DateTime.now - 2.hour
+    curr_event_2.end_date = DateTime.now + 2.hour
+    curr_event_2.save
+
+    # Get the list of current events from the function we're testing
+    current_events = viktor.current_events
+
+    # We created two test events as current events, so make sure we only got
+    # two events back from the function call
+    assert_equal(2, current_events.length, "Got wrong number of current events compared to the test data! (should be two)")
+
+    # Make sure we got the right event as the first event (to check sorting order,
+    # and make sure that we didn't get any events returned as "current events" that
+    # shouldn't be)
+    assert_equal(curr_event_1, current_events[0], "First current event is wrong; either we got a wrong current event, or the current events are in the wrong order.")
+
+    # Make sure that second event is also the right one, and in the right order
+    assert_equal(curr_event_2, current_events[1], "Second current event is wrong; either we got a wrong current event, or the current events are in the wrong order.")
   end
 end
