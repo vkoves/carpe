7b0535d43c6b98bb19ef9c151e802b81ae3d7da9
Matthew Spero <Watercycle@email.com>
2017-03-29 00:52:02 -0500

    Changed user custom_url to use an empty string as its default value

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320032431) do
+ActiveRecord::Schema.define(version: 20170329054137) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -130,7 +130,7 @@ ActiveRecord::Schema.define(version: 20170320032431) do
     t.string   "banner_content_type"
     t.integer  "banner_file_size"
     t.datetime "banner_updated_at"
-    t.string   "custom_url"
+    t.string   "custom_url",             default: ""
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
