18eff4328fb13564b561c3086d85db11d7e1b17b
Viktor Koves <viktorkoves@gmail.com>
2017-07-03 22:47:51 -0500

    Modularized user_adder into a partial The user_adder is now very easy to implement and relatively modular

---
+++ app/assets/javascripts/partials/user-adder.js
@@ -0,0 +1,37 @@
+// Tokenizer implementation for the user entry
+function initializeUserAdder(selector)
+{
+	$(selector).tokenInput("/search_users.json", {
+		crossDomain: false,
+		placeholder: "Add people",
+		searchDelay: 0,
+		animateDropdown: false,
+		onAdd: function(item)
+		{
+			var usersSelected = this.tokenInput("get");
+
+			var itemCount = 0; //how many times this item occurs
+			for(var i = 0; i < usersSelected.length; i++)
+			{
+				if(usersSelected[i].id == item.id) //if this is the item
+					itemCount++; //increment
+			}
+
+			if(itemCount > 1) //if this is a duplicate
+			{
+				this.tokenInput("remove", {id: item.id}); //remove all copies
+				this.tokenInput("add", item); //and add it back
+			}
+		},
+		resultsFormatter: function(element)
+		{
+			img_url = element.image_url || "https://www.gravatar.com/avatar/?d=mm";
+			return "<li>" + "<div class='avatar search-avatar'><img src='" + img_url + "'></div><div class='name'>" + element.name + "</div></li>";
+		},
+		tokenFormatter: function(element)
+		{
+			img_url = element.image_url || "https://www.gravatar.com/avatar/?d=mm";
+			return "<li>" + "<div class='avatar'><img src='" + img_url + "'></div><p>" + element.name + "</p></li>";
+		}
+	});
+}
