ada1d66c699cc4f3b4533fdc7a7037164c0413bf
Matthew Spero <Watercycle@email.com>
2017-07-05 03:35:09 -0500

    Added application controller test (but no tests)

---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ApplicationControllerTest < ActionController::TestCase
+  include Devise::Test::ControllerHelpers
+
+  test "searching works" do
+    flunk "duplicate code between user 'search' and application 'search_core'"
+  end
+end
