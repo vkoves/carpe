9bddbb51580f3393ed34cc68dc52dd7939f7de39
Matthew Spero <Watercycle@email.com>
2017-07-06 23:44:11 -0500

    Removed flunking application controller test
    The original reason it was added in has been addressed in a pull
    request. Actual application controller tests will be added in at a
    later date.

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -4,6 +4,5 @@ class ApplicationControllerTest < ActionController::TestCase
   include Devise::Test::ControllerHelpers
 
   test "searching works" do
-    flunk "duplicate code between user 'search' and application 'search_core'"
   end
 end
