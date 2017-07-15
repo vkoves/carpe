d709feb76f5ee32d5d1e216157b9638c3a260b94
Matthew Spero <Watercycle@email.com>
2017-07-15 18:18:59 -0500

    Added custom_url and banner to groups schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714132740) do
+ActiveRecord::Schema.define(version: 20170715231403) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -70,7 +70,12 @@ ActiveRecord::Schema.define(version: 20170714132740) do
     t.datetime "updated_at",        null: false
     t.string   "banner_image_url"
     t.boolean  "posts_preapproved"
-    t.integer  "privacy",           default: 0
+    t.integer  "privacy",             default: 0
+    t.string   "custom_url"
+    t.string   "banner_file_name"
+    t.string   "banner_content_type"
+    t.integer  "banner_file_size"
+    t.datetime "banner_updated_at"
   end
 
   create_table "notifications", force: :cascade do |t|
