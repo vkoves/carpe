fa668993ea9780c33e5d041eddf148f3401b4d29
Matthew Spero <Watercycle@email.com>
2017-07-28 16:45:48 -0500

    Added package.json and PhantomJS back in.
    Yes. First I delete your pull request and now I revert some of your
    reverts.
    
    PhantomJS is (based on some dirty benchmarks I did) about 3 times faster
    than nightmare. The only catch is that automatically killing the PhantomJS
    server is not straightforward or cross-platform, so this solution makes use
    of 'node' globally. Node is typically bundled with npm, but on the off chance
    somebody does not have node or want to install it, they're free to copy paste
    the commands from stop-phantom.js into package.json.
    
    Lastly, it is indeed recommended to commit the package-lock.json similar to
    Ruby's gemlock file.

---
+++ stop-phantomjs.js
@@ -0,0 +1,6 @@
+require("child_process").exec(process.platform === 'win32' ?
+	"taskkill /F /IM phantomjs.exe /T" :
+	"killall phantomjs",
+    function (error, stdout, stderr) {
+        console.log(stdout);
+});
