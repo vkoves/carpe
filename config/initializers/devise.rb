5e7034d0928971ca9f3bad30b28e2e84875d4392
Viktor Koves <viktorkoves@gmail.com>
2017-11-18 22:28:35 -0600

    Fixed improper format for changing devise email from

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  default :from => 'Carpe <do-not-reply@carpe.us>'
+  config.mailer_sender = 'Carpe <do-not-reply@carpe.us>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
