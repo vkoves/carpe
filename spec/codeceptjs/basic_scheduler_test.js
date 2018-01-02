24c2035a7cf3fbe4c590ea81716dfe7d63b8b4d7
Viktor Koves <viktorkoves@gmail.com>
2018-01-01 21:53:44 -0600

    Added schedule pageObject for moving code together to simplify schedule page testing

--- spec/codeceptjs/basic_scheduler_test.js
+++ spec/codeceptjs/basic_scheduler_test.js
@@ -1,12 +1,7 @@
 
-Feature('scheduler loads after sign in');
+Feature('Scheduler loads after sign in');
 
-Scenario('test something', (I) => {
-	I.amOnPage('/schedule');
-	I.seeElement('.alert-holder .alert'); // an error should be thrown for visiting a page that requires authentication
-	I.seeInCurrentUrl('/users/sign_in');
-	I.login('user1@example.com', 'password');
-	I.amOnPage('/schedule'); // now that the user is logged in, going to the schedule page should actually go to the schedule
-	I.seeInCurrentUrl('/schedule');
+Scenario('can load schedule page', (I, schedulePage) => {
+	schedulePage.loginToSchedule(); // loads schedule page and logs in after prompt (see pages/schedule.js for details)
 	I.seeElement('#sch-main'); // check for schedule element
 });
---
+++ spec/codeceptjs/pages/schedule.js
@@ -0,0 +1,21 @@
+// Documentation on CodeceptJS page objects:
+// http://codecept.io/pageobjects/
+
+'use strict';
+
+let I;
+
+module.exports = {
+
+  _init() {
+    I = require('../steps_file.js')();
+  },
+
+  loginToSchedule() {
+    I.amOnPage('/schedule');
+    I.seeElement('.alert-holder .alert'); // an error should be thrown for visiting a page that requires authentication
+    I.seeInCurrentUrl('/users/sign_in');
+    I.login('user1@example.com', 'password');
+    I.amOnPage('/schedule'); // now that the user is logged in, going to the schedule page should actually go to the schedule
+  }
+}
