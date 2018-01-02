8bce1ac83bfdff74487e535e7da486214b67f40a
Viktor Koves <viktorkoves@gmail.com>
2018-01-01 23:19:25 -0600

    Made UI Manager mark visible .overlay-box elements and added basic cateogry creation test

--- spec/codeceptjs/basic_scheduler_test.js
+++ spec/codeceptjs/basic_scheduler_test.js
@@ -5,3 +5,11 @@ Scenario('can load schedule page', (I, schedulePage) => {
 	schedulePage.loginToSchedule(); // loads schedule page and logs in after prompt (see pages/schedule.js for details)
 	I.seeElement('#sch-main'); // check for schedule element
 });
+
+
+Scenario('test creating category', (I, schedulePage) => {
+	schedulePage.loginToSchedule(); // loads schedule page
+	I.dontSeeElement('#cat-overlay-box.visible'); // ensure category overlay starts hidden
+	I.click('.cat-add'); // click New Category button
+	I.seeElement('#cat-overlay-box.visible'); // check the overlay is present
+});
