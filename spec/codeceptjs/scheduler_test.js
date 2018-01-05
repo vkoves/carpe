cea27f9ab4a496f161206d4b5fd76ae7a36c0da9
Viktor Koves <viktorkoves@gmail.com>
2018-01-04 22:49:05 -0600

    Specified Node version to be 8 and wrote improved category creation test using async

--- spec/codeceptjs/scheduler_test.js
+++ spec/codeceptjs/scheduler_test.js
@@ -7,11 +7,14 @@ Scenario('can load schedule page', (I, schedulePage) => {
 });
 
 
-Scenario('can create category', (I, schedulePage) => {
+Scenario('can create category', async (I, schedulePage) => {
 	schedulePage.loginToSchedule(); // loads schedule page
 	I.dontSeeElement(schedulePage.overlays.category); // ensure category overlay starts hidden
+
+	let previousCategoryCount = await I.grabNumberOfVisibleElements('#sch-sidebar .category:not([data-id="-1"])'); // count existing categories
 	I.click('.cat-add'); // click New Category button
 	I.seeElement(schedulePage.overlays.category); // check the overlay is present
+	I.seeNumberOfVisibleElements('#sch-sidebar .category:not([data-id="-1"])', previousCategoryCount + 1); // verify there is one more category than before
 });
 
 Scenario('can edit category', (I, schedulePage) => {
