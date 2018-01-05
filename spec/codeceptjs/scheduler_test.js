b802e9ccaa3f703e49b00f0224ac500343d00a10
Viktor Koves <viktorkoves@gmail.com>
2018-01-04 23:30:40 -0600

    Added delete category test

--- spec/codeceptjs/scheduler_test.js
+++ spec/codeceptjs/scheduler_test.js
@@ -19,6 +19,23 @@ Scenario('can create category', async (I, schedulePage) => {
 
 Scenario('can edit category', (I, schedulePage) => {
 	schedulePage.loginToSchedule(); // loads schedule page
-	I.click('#sch-sidebar .category:not([data-id="-1"]) .sch-evnt-edit-cat'); // click
+	I.click('#sch-sidebar .category:not([data-id="-1"]) .sch-evnt-edit-cat'); // click edit on first category
 	I.seeElement(schedulePage.overlays.category); // check the overlay is present
+});
+
+Scenario('can delete category with confirmation', async (I, schedulePage) => {
+	schedulePage.loginToSchedule(); // loads schedule page
+	let previousCategoryCount = await I.grabNumberOfVisibleElements('#sch-sidebar .category:not([data-id="-1"])'); // count existing categories
+
+	I.dontSeeElement('#overlay-confirm.visible'); // ensure confirm overlay starts hidden
+
+	I.click('#sch-sidebar .category:not([data-id="-1"]) .sch-evnt-del-cat'); // click delete on first category
+
+	I.seeElement('#overlay-confirm.visible'); // verify confirmation overlay appears
+
+	I.click('#overlay-confirm #confirm') // click confirm on the deletion
+
+	I.dontSeeElement('#overlay-confirm.visible'); // ensure confirm overlay is hidden again
+
+	I.seeNumberOfVisibleElements('#sch-sidebar .category:not([data-id="-1"])', previousCategoryCount - 1); // verify there is one less category than before
 });
