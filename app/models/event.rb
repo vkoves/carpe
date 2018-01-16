b9c0a38c9dc79652a00a2393d04693c40238e93a
Watercycle <Watercycle@email.com>
2018-01-16 11:19:09 -0600

    Updated events_in_range (yearly fix + DST + new tests)

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,32 +20,31 @@ class Event < ApplicationRecord
     repeat.present? and repeat != 'none'
   end
 
-  # Returns true if this event is currently on break at the given time, false otherwise.
+  # Returns true if this event is on break at the given time, false otherwise.
   def on_break?(datetime)
     all_repeat_exceptions.any? { |brk| datetime.between? brk.start, brk.end }
   end
 
-  # Returns true if this event is currently repeating at the given time, false otherwise.
-  def repeating?(datetime)
-    return false if repeat_start.present? and datetime < repeat_start
-    return false if repeat_end.present? and datetime > repeat_end
-    return false if on_break? datetime
+  # Returns how long the event goes on for in seconds
+  def duration
+    end_date - date
+  end
+
+  # Returns true if this event can occur at the given time, false otherwise.
+  def can_occur_on?(day)
+    return false if on_break? day
+    return true unless repeats?
+    return false if repeat_start.present? and day < repeat_start
+    return false if repeat_end.present? and day > repeat_end
 
     true
   end
 
-  # Returns copies of the event on every day it applies to between /start/ and /end/.
-  def events_in_range(start_datetime, end_datetime)
-    unless repeats?
-      # just return the current event, as it's the only relevant one.
-      return date.between?(start_datetime, end_datetime) ? [self] : []
-    end
-
-    # get all candidate dates for the event (ignores exceptions and repeat date range)
-    candidate_dates = dates_in_range_with_repeat start_datetime, end_datetime
-
-    # now only take candidates actually in this event's repeat range that aren't on break.
-    candidate_dates.map { |day| repeat_clone day if repeating? day }.compact
+  # Returns an array of events on every day that this event applies to between the given
+  # /start/ and /end/ times.
+  def events_in_range(start, finish, time_zone="UTC")
+    event_days = dates_in_range_with_repeat(start, finish, time_zone)
+    event_days.select { |day| can_occur_on? day }.map { |day| repeat_clone day }
   end
 
   #returns all repeat_exceptions that apply to this event, a combination of event and category level ones
@@ -81,84 +80,45 @@ class Event < ApplicationRecord
   private
 
   def repeat_clone(date)
-    self_date = self.date
-    self_dst = self_date.utc.in_time_zone("Central Time (US & Canada)").dst? #get whether this event is in daylight savings time
-    now_dst = Time.now.utc.in_time_zone("Central Time (US & Canada)").dst? #get whether the current time is in daylight savings
-    one_hour = 1.hour
+    new_event = self.dup
+    new_event.attributes = { date: date, end_date: date + duration }
+    new_event
+  end
 
-    new_event = self.dup #duplicate the base element without creating a database clone
-    new_start_date = self_date.change(day: date.day, month: date.month, year: date.year) #and determine the new start date
-    new_end_date = new_start_date + (self.end_date - self_date) #determine proper end datetime by adding event duration to the proper start
+  # Returns the first date this event will repeat for a given
+  # /start/ time, /finish/ time, and repeat duration (i.e. /step/size).
+  # If this event does not repeat during the given time, /finish/ will be returned instead.
+  def first_repeat(start, finish, step)
+    duration = (start.to_time - date.to_time).abs
 
-    if self_dst != now_dst #if the date is in daylight savings, but we are not, or vice versa
-      if self_dst
-        new_event.date = new_start_date + one_hour
-        new_event.end_date = new_end_date + one_hour
-      else
-        new_event.date = new_start_date - one_hour
-        new_event.end_date = new_end_date - one_hour
-      end
+    if start >= date
+      first_repeat_date = date + (duration / step).ceil * step
     else
-      new_event.date = new_start_date
-      new_event.end_date = new_end_date
+      first_repeat_date = date - (duration / step).floor * step
     end
 
-    return new_event
+    first_repeat_date.between?(start, finish) ? first_repeat_date : finish
   end
 
-  # Returns an enumerator of all the dates between /start_date/ and /end_date/
-  # that this custom repeating event will occur on. This is a helper method for
-  # dates_in_range_with_repeat.
-  #
-  # For example, if an event occurs on 7/11/17 and repeats every 3 days - and
-  # we want all the dates this applies to between 7/04/17 - 7/15/17. Then this
-  # method will return (7/5, 7/8, 7/11, 7/14). It does so by finding the
-  # day that the repeating event will occur on in the specified range
-  # and stepping through dates until it has reached the given 'end date'.
-  def select_custom_repeat_dates(start_date, end_date)
-    repeat_data = repeat.split("-")
-    repeat_num = repeat_data[1].to_i # repeat this event every n
-    repeat_unit = repeat_data[2] # days/weeks/months/years
+  # Returns an array of dates that this event object may apply to between
+  # the given /start/ and /finish/ times.
+  def dates_in_range_with_repeat(start, finish, time_zone="UTC")
+    start = start.in_time_zone(time_zone)
 
-    dates = range(start_date, end_date, 1.day)
-    first_weekday = dates.find { |day| (day.wday - date.wday) % repeat_num == 0 }
-
-    case repeat_unit
-    when 'days'   then range first_weekday, end_date, repeat_num.days
-    when 'weeks'  then range first_weekday, end_date, repeat_num.weeks
-    when 'months' then range first_weekday, end_date, repeat_num.months
-    when 'years'  then range first_weekday, end_date, repeat_num.years
-    end
-  end
-
-  # Returns an array of dates from taking only certain weekdays from the array
-  # of /dates/. 'repeat' should look something along the lines of "certain_days-1,3,4,5".
-  # 1 meaning monday, 2 tuesday, ..., 6 saturday, and 0 sunday.
-  # This is a helper method for dates_in_range_with_repeat.
-  def select_certain_dates(dates)
-    weekday_nums = repeat.split('-')[1].split(',').map(&:to_i)
-    dates.select { |day| weekday_nums.include?(day.wday) }
-  end
-
-  # Similar to select_custom_repeat_dates, except the repeat dates are predefined.
-  # This is a helper method for dates_in_range_with_repeat.
-  def select_repeat_dates(start_date, end_date, step)
-    dates = range(start_date, end_date, 1.day)
-    first_weekday = dates.find { |day| day.wday == date.wday }
-    range first_weekday, end_date, step
-  end
-
-  # Takes a DateTime /start_date/ and a DateTime /end_date/ and returns
-  # a collection of dates in this range that the current event object applies to.
-  def dates_in_range_with_repeat(start_date, end_date)
     case repeat
-    when 'daily'        then range start_date, end_date, 1.day
-    when 'weekly'       then select_repeat_dates start_date, end_date, 1.week
-    when 'monthly'      then select_repeat_dates start_date, end_date, 1.month
-    when 'yearly'       then select_repeat_dates start_date, end_date, 1.year
-    when /certain_days/ then select_certain_dates range(start_date, end_date, 1.day)
-    when /custom/       then select_custom_repeat_dates start_date, end_date
-    else [date] # this event doesn't repeat
+    when 'daily'   then range start, finish, 1.day
+    when 'weekly'  then range first_repeat(start, finish, 1.week), finish, 1.week
+    when 'monthly' then range first_repeat(start, finish, 1.month), finish, 1.month
+    when 'yearly'  then range first_repeat(start, finish, 1.year), finish, 1.year
+    when /custom/
+      _, repeat_every, repeat_unit = repeat.split("-")
+      step = repeat_every.to_i.send(repeat_unit)
+      range first_repeat(start, finish, step), finish, step
+    when /certain_days/
+      weekdays = repeat.split('-')[1].split(',').map(&:to_i)
+      range(start, finish, 1.day).select { |day| weekdays.include?(day.wday) }
+    else
+      date.between?(start, finish) ? [date] : [] # this event doesn't repeat
     end
   end
 end
--- lib/utilities.rb
+++ lib/utilities.rb
@@ -5,17 +5,17 @@
 
 module Utilities
 
-  # Returns an enumerator of values beginning at /start/ and ending at /finish/
+  # Returns an enumerator of values beginning at /start/ going up to /finish/
   # based on the given /step/ size.
   #
-  # For example, range(1, 5, 2).to_a = [1, 3, 5]
+  # For example, range(1, 5, 2).to_a = [1, 3]
   # The most common use case for this method is as an alternative to the
   # Date.Step(Date, step) method which uses a fixed step-size.
   #
   # Whereas range(DateTime.new(2013, 1, 1), DateTime.new(2014, 1, 1), 1.month)
   # properly returns the first day of each month rather than using a fixed 30 days.
   def range(start, finish, step)
-    return [] if start.nil? || finish.nil?
+    return [] if start >= finish
 
     Enumerator.new do |y|
       y << start
