9a8ff17dc7d8ca549490cad3ad83c9b472043c14
Watercycle <Watercycle@email.com>
2018-01-31 06:00:33 -0600

    Removed deadlock from acceptance test script
    Using `exec` and `execSync`, the CodeceptJs process would deadlock on
    failing tests. Detaching the server resolves this... because magic

--- run-codeceptjs-with-server.js
+++ run-codeceptjs-with-server.js
@@ -1,4 +1,4 @@
-const {exec, execSync} = require('child_process')
+const {spawn, spawnSync} = require('child_process')
 const kill = require('tree-kill')
 const isConnected = require('tcp-ping').probe
 
@@ -7,18 +7,20 @@ isConnected('localhost', 3000, (error, connected) => {
 		console.error("Rails server must be started before running acceptance tests.")
 	} else {
 		console.log("Starting Selenium server...")
-		const server = exec('selenium-standalone start', {stdio: 'inherit'})
+		const server = spawn('selenium-standalone', ['start'], {stdio: 'ignore', detached: true})
 
-		console.log("Seeding database with test data...")
-		execSync('bundle exec rails db:seed', {stdio: 'inherit'})
+		try {
+			console.log("Seeding database with test data...")
+			spawnSync('bundle', ['exec', 'rails', 'db:seed'], {stdio: 'inherit'})
 
-		console.log("Running CodeceptJs...")
-		execSync('codeceptjs run --reporter mochawesome', {stdio: 'inherit'})
+			console.log("Running CodeceptJs...")
+			spawnSync('codeceptjs', ['run', '--reporter', 'mochawesome'], {stdio: 'inherit'})
 
-		console.log("Closing Selenium server...")
-		kill(server.pid)
-
-		console.log("Cleaning up database test data...")
-		execSync('rails acceptance_cleanup', {stdio: 'inherit'})
+			console.log("Cleaning up database test data...")
+			spawnSync('rails', ['acceptance_cleanup'], {stdio: 'inherit'})
+		} finally {
+			console.log("Closing Selenium server...")
+			kill(server.pid)
+		}
 	}
 })
