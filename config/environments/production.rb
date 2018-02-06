9cfca5520f63e352e5242554570abbc5811d8cef
Watercycle <Watercycle@email.com>
2018-02-06 13:24:11 -0600

    Added host parameter to default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { host: ENV["HOST"] }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -87,5 +89,4 @@ Rails.application.configure do
       s3_region: ENV.fetch('AWS_REGION'),
     }
   }
-
 end
