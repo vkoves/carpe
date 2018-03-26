2edbc88b91cc4e4b82a3d367a51bde45bdb87cd9
Watercycle <Watercycle@email.com>
2018-03-25 20:55:51 -0500

    Fixed typo in migration

--- db/migrate/20180219082149_make_follower_request_notifications_from_relationships.rb
+++ db/migrate/20180219082149_make_follower_request_notifications_from_relationships.rb
@@ -1,9 +1,9 @@
 class MakeFollowerRequestNotificationsFromRelationships < ActiveRecord::Migration[5.1]
   def self.up
     Relationship.where(confirmed: false).find_each do |relationship|
-        Notification.find_or_create_by!(receiver_id: relationship.followed_id,
-                                        sender_id: relationship.follower_id,
-                                        object_id: relationship.id,
+        Notification.find_or_create_by!(receiver: relationship.followed,
+                                        sender: relationship.follower,
+                                        entity: relationship,
                                         event: Notification.events[:follow_request])
     end
   end
