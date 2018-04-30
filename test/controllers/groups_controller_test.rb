6136755c9412b86d412120148a8b37f9bc07f465
kabirnbaum <kylewdgb69@gmail.com>
2018-04-29 17:28:40 -0700

    added more tests

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -3,6 +3,30 @@ require 'test_helper'
 class GroupsControllerTest < ActionController::TestCase
   include Devise::Test::ControllerHelpers
 
+  test "user must be signed in to see groups" do
+    get :index
+    assert_response :redirect
+  end
+
+  test "signed in users can see groups" do
+    sign_in users(:norm)
+    get :index
+    assert_response :success
+  end
+
+  test "user must be signed in to see public group" do
+    get :show,
+      params: {group:one}
+    assert_response :redirect
+  end
+
+  test "signed in users can see a public group" do
+    sign_in users(:norm)
+    get :show,
+      params: {group:one}
+    assert_response :redirect
+  end
+
   test "user must be signed in to create a group" do
     get :create
     assert_response :redirect
