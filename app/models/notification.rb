8d669587a45021f20c17b4a452af79914f7a84db
Watercycle <Watercycle@email.com>
2018-07-03 21:18:15 -0500

    Fixed unique constraint on notifications
    This also adds `dependent: destroy` as the notifications table's purpose isn't to represent a history of events.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,10 +1,10 @@
 class Notification < ApplicationRecord
   enum event: { system_message: 0, follow_request: 1, user_message: 2 }
 
-  belongs_to :entity, polymorphic: true
+  belongs_to :entity, polymorphic: true, dependent: :destroy
 
-  belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
-  belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
+  belongs_to :sender, class_name: 'User', foreign_key: 'sender_id', dependent: :destroy
+  belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id', dependent: :destroy
 
   scope :unread, -> { where(viewed: false) }
 end
