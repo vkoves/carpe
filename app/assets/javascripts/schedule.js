d051c22fa70965fb04a0c2944a15d7a689a313fe
kabirnbaum <kylebirnbaum@gmail.com>
2018-08-25 15:34:50 -0700

    finishing touches

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -1620,7 +1620,7 @@ function getStartDate(dateObj, useMonth)
 	{
 		lastMonth = true;
 	}
-
+ 
 	copyDate.setDate(startDate);
 
 	return {startDate: copyDate, lastMonth: lastMonth}
@@ -1644,8 +1644,8 @@ function populateEvents()
 	function place(eventObject, visibleDate)
 	{
 		var color = categories[eventObject.categoryId].color;
-		var currentElem = eventObject.tempElement.clone();
-
+    var currentElem = eventObject.tempElement.clone();
+    
 		if(viewMode == "week")
 		{
 			// Setup the UI element's color, text, and height to represent the schedule item
@@ -1974,6 +1974,7 @@ function editEvent(elem)
     $("#cat-title-selector").change(function() {
       var val = $(this).val();
       currEvent.setCategory(val)
+      $(".sch-evnt[event-id='" + currEvent.eventId + "'], #overlay-color-bar").css("background-color", categories[currEvent.categoryId].color)
     })
 
 		//Select the proper repeat button
