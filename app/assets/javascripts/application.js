ca60473647139d73b82c49b53bc5a31d954e7b21
Viktor Koves <viktorkoves@gmail.com>
2018-08-31 23:24:36 -0500

    Fixed application.js JSHint failures Also added .jshintrc and set it so we use ES6

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -63,7 +63,7 @@ function ready()
 
 	initializeEventListeners();
 	keyboardShortcutHandlers();
-};
+}
 
 /**
  * Adds event listeners (e.g., onclick) to elements throughout the site
@@ -132,47 +132,47 @@ function initializeEventListeners()
 
 	// Follow button
     $(document).on('click', '.js-follow-user', function() {
-        $button = $(this)
+        $button = $(this);
 
         $.post($(this).attr('href'), function() {
             // convert from a button into a pending state span
-            $span = $(`<span class="friend-label">Follow</span>`).replaceAll($button);
-            fadeToText($span, "Pending") // "Follow" -> "Pending"
-        })
+            $span = $('<span class="friend-label">Follow</span>').replaceAll($button);
+            fadeToText($span, "Pending"); // "Follow" -> "Pending"
+        });
 
-        return false
+        return false;
     });
 
     // Unfollow button
     $('.js-unfollow-user').click(function() {
-        $button = $(this)
+        $button = $(this);
 
         $.ajax({
             url: $(this).attr('href'),
             type: 'DELETE'
         }).done(function(data) {
             // convert from an unfollow button into a follow button
-            $button.off() // reset events tied to this element (like hovering)
-            $button.attr('href', data['new_link'])
-            fadeToText($button, 'Follow')
-            $button.attr('class', 'green button js-follow-user')
-        })
+            $button.off(); // reset events tied to this element (like hovering)
+            $button.attr('href', data.new_link);
+            fadeToText($button, 'Follow');
+            $button.attr('class', 'green button js-follow-user');
+        });
 
-        return false
+        return false;
     });
 
     // Used on profile panel.
     $("#friend-list .js-unfollow-user").click(function() {
-        $(this).closest('.user-listing').fadeOut()
-        return false
-    })
+        $(this).closest('.user-listing').fadeOut();
+        return false;
+    });
 
     // Hovering over a profile 'Following' button transforms it into an 'Unfollow' button
     // Sadly, this can't be accomplished with CSS.
     $(".profile-header .button.js-unfollow-user").hover(function() {
-        $(this).text("Unfollow") // mouse in
+        $(this).text("Unfollow"); // mouse in
     }, function() {
-        $(this).text("Following") // mouse out
+        $(this).text("Following"); // mouse out
     });
 
 	$("#notif-panel").on("ajax:success", "a", function()
@@ -182,25 +182,25 @@ function initializeEventListeners()
 
 		const $notifCard = $(this).parents(".notif");
 		$notifCard.delay(150).fadeOut(handleNotificationClosed);
-	})
+	});
 
 	//Promote buttons POST completion
 	$(".promotion span").parent().bind('ajax:success', function(event, data, status, xhr){
-		if(data && data["action"] && data["action"] === "promote" || data["action"] === "demote")
+		if(data && data.action && data.action === "promote" || data.action === "demote")
 		{
 			//since the ajax:success is called on every promotion button, only run code if this is the one that was clicked
-			if($(this).attr("uid") == parseInt(data["uid"]))
+			if($(this).attr("uid") == parseInt(data.uid))
 			{
 				var span = $(this).find("span"); //get the span tag in this button
 
 				if($(this).hasClass("red")) //if the user was demoted (the button was red)
 				{
-					$(this).attr("href", data["new_href"]); //remove demote parameter
+					$(this).attr("href", data.new_href); //remove demote parameter
 					fadeToText(span, "Promote"); //and fade to Promote text
 				}
 				else //if the user was promoted (the button was not red)
 				{
-					$(this).attr("href", data["new_href"]); //add the demote parameter
+					$(this).attr("href", data.new_href); //add the demote parameter
 					fadeToText(span, "Demote"); //and fade to Demote text
 				}
 				$(this).toggleClass("red"); //and toggle class red
@@ -248,8 +248,8 @@ function keyboardShortcutHandlers()
 		var ctrl = e.ctrlKey;
 		var alt = e.altKey;
 
-		if((shift && pressed("/"))
-			|| (ctrl && pressed("/")))
+		if((shift && pressed("/")) ||
+			(ctrl && pressed("/")))
 		{
 			e.preventDefault();
 
@@ -286,7 +286,7 @@ function keyboardShortcutHandlers()
 		{
 			e.preventDefault();
 			if(viewMode == "week")
-				initializeMonthlyView()
+				initializeMonthlyView();
 			else
 				initializeWeeklyView();
 		}
@@ -431,7 +431,7 @@ function printNotification(text, hideTime)
 	var options = {
 		body: text,
 		icon: 'assets/images/CarpeIcon.png',
-	}
+	};
 	var notification = new Notification("Carpe", options);
 	if(hideTime)
 		setTimeout(notification.close.bind(notification), hideTime); //close this notification in 2000ms or 2 seconds
@@ -451,7 +451,7 @@ function handleNotificationClosed()
 	if($(".notif:visible").length == 0) // if no notifications left
 	{
 		$("#notif-title").fadeOut(function() {
-			$("#no-notifs").fadeIn()
+			$("#no-notifs").fadeIn();
 		});
 	}
 }
