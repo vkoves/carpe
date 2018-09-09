50ed1ba00b29e0b986c6e10604833c90d16da36a
Viktor Köves <viktorkoves@gmail.com>
2018-09-09 15:12:46 -0500

    Added ScheduleItem first test

--- spec/javascripts/schedule_spec.js
+++ spec/javascripts/schedule_spec.js
@@ -8,4 +8,24 @@ describe('Schedule', function() {
 	it('Should be safe to leave', function() {
 		expect(isSafeToLeave()).to.be.true;
 	});
+
+	describe('ScheduleItem', function() {
+		beforeEach(function() {
+			// Create a ScheduleItem
+			this.testTempId = 123;
+			groupID = null; // expected from template
+			this.schItem = new ScheduleItem();
+			this.schItem.tempId = this.testTempId;
+
+			// Add it to scheduleItem
+			scheduleItems[this.testTempId] = this.schItem;
+		});
+
+		describe('.destroy()', function() {
+			it('Removes it from scheduleItems', function() {
+				this.schItem.destroy();
+				expect(scheduleItems[this.testTempId]).to.be.undefined;
+			});
+		});
+	});
 });
