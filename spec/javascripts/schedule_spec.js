8a3ba88d898c3ada6ab71096569e04efed97644a
Viktor Koves <viktorkoves@gmail.com>
2018-09-16 22:23:11 -0500

    Improved schedule_spec usage of groupID

--- spec/javascripts/schedule_spec.js
+++ spec/javascripts/schedule_spec.js
@@ -15,9 +15,7 @@ describe('Schedule', () => {
 
 	describe('ScheduleItem', () => {
 		beforeEach(() => {
-			// groupId is already defiend, so we ignore ESLint on it
-			/* eslint-disable-next-line no-undef */
-			groupID = null; // instantiated in template, but used outside of here
+			window.groupID = null; // used in scheduler, normally from template
 
 			// Create a ScheduleItem
 			this.testTempId = 123;
