e50c51727f115e302e1365c03ef592ac71d2d00a
Watercycle <Watercycle@email.com>
2018-09-19 15:47:25 -0500

    Capybara Chrome options final

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,20 +2,23 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
-  # Chrome Options Explained:
-   
-  # disable-dev-shm-usage:
-  # Prevents Headless Chrome from blowing Docker memory limits on Travis
-  # Full explanation: https://github.com/GoogleChrome/puppeteer/blob/v1.0.0/docs/troubleshooting.md#tips
+  # Chrome Options Explained
+  #
+  # headless:
+  # Runs Chrome without launching a window. You can disable this if you want
+  # to watch the tests perform their actions.
   #
   # no-sandbox:
-  # When running in a headless Chrome in a Docker container without a user
+  # When running in a headless Chrome in a Docker container without a user,
   # this option is required.
+  #
+  # disable-dev-shm-usage:
+  # Prevents Headless Chrome from blowing Docker memory limits on Travis
   #
   Capybara.register_driver :custom_chrome do |app|
     capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
       chromeOptions: {
-        args: %w[headless window-size=1920,1080]
+        args: %w[headless window-size=1920,1080 no-sandbox disable-dev-shm-usage]
       }
     )
 
