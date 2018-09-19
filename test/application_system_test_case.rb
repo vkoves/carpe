2757af75a52efb2e096daad8945b6a7addac5b27
Watercycle <Watercycle@email.com>
2018-09-19 15:37:43 -0500

    Capybara chrome options travis smoke test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,13 +1,21 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+
+  # Chrome Options Explained:
+   
+  # disable-dev-shm-usage:
+  # Prevents Headless Chrome from blowing Docker memory limits on Travis
+  # Full explanation: https://github.com/GoogleChrome/puppeteer/blob/v1.0.0/docs/troubleshooting.md#tips
+  #
+  # no-sandbox:
+  # When running in a headless Chrome in a Docker container without a user
+  # this option is required.
+  #
   Capybara.register_driver :custom_chrome do |app|
     capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
       chromeOptions: {
-        args: %w[headless disable-gpu
-                 window-size=1920,1080
-                 remote-debugging-port=9222
-                 no-sandbox disable-dev-shm-usage]
+        args: %w[headless window-size=1920,1080]
       }
     )
 
