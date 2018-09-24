04aac26b9124bd5faa492fe7abea442cfae89618
Watercycle <Watercycle@email.com>
2018-09-23 19:10:17 -0500

    Cleaned up application helper

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -4,6 +4,11 @@
 # namespace collisions back to this file.
 
 module Utilities
+  # contains pluralize method
+  include ActionView::Helpers::TextHelper
+
+  # just a nicer alias
+  INFINITY = Float::INFINITY
 
   # Returns an enumerator of values beginning at /start/ going up to /finish/
   # based on the given /step/ size.
@@ -24,6 +29,50 @@ module Utilities
       end
     end
   end
+
+  # Returns an array of units from ActiveSupport::Duration.
+  # Amazingly, ActiveRecord::Duration doesn't already provide this.
+  def duration_parts(duration)
+    dur = duration.abs
+
+    secs = dur.round
+    mins = (dur / 1.minute).round
+    days = (dur / 1.day).round
+    months = (dur / 1.month).round
+
+    [secs, mins, days, months]
+  end
+
+  # rubocop: disable MethodLength, AbcSize, CyclomaticComplexity
+
+  # Returns a formattable string representing the distance between the current
+  # time and the given time. For example, "3 minutes ago (5:50 PM)".
+  def relative_time(to_time)
+    duration = to_time - Time.current # it's in seconds
+    secs, mins, days, months = duration_parts(duration)
+    time = "%-l:%M %p" # time format
+
+    case duration
+    when -INFINITY..-1.month   then "#{pluralize months, 'month'} ago"
+    when -1.month..-1.week     then "#{pluralize days, 'day'} ago"
+    when -1.week..-2.days      then "last %A at #{time}"
+    when -2.days..-1.day       then "yesterday at #{time}"
+    when -1.day..-1.hour       then time
+    when -1.hour..-1.minute    then "#{pluralize mins, 'minute'} ago (#{time})"
+    when -1.minute...0.seconds then "#{pluralize secs, 'second'} ago (#{time})"
+    when 0.seconds             then "right now"
+    when 0.seconds...1.minute  then "#{pluralize secs, 'second'} from now (#{time})"
+    when 1.minute...1.hour     then "#{pluralize mins, 'minute'} from now (#{time})"
+    when 1.hour...1.day        then time
+    when 1.day...2.days        then "tomorrow at #{time}"
+    when 2.days...1.week       then "%A at #{time}"
+    when 1.week...1.month      then "#{pluralize days, 'day'} from now"
+    when 1.month...INFINITY    then "#{pluralize months, 'month'} from now"
+    else raise "Congratulations, you won a bug!"
+    end
+  end
+
+  # rubocop: enable MethodLength, AbcSize, CyclomaticComplexity
 end
 
 # Automatically include this modules methods in the global namespace.
---
+++ test/libs/utilities_test.rb
@@ -0,0 +1,128 @@
+require 'test_helper'
+require 'utilities'
+
+class UtilitiesTest < ActiveSupport::TestCase
+  def setup
+    # NOTE: Time.now and friends are all based on this date.
+    # This will help keep tests predictable.
+    new_current_time = Time.parse('15th May 2018 5:00:00 PM') # a monday
+    travel_to new_current_time
+  end
+
+  # ----------------------------------
+  # #relative_time - Past Times
+  # ----------------------------------
+
+  test "#relative_time works (> 1 month ago)" do
+    test_time = Time.parse('5th April 2018 5:30:00 PM')
+    str = test_time.strftime(relative_time(test_time))
+
+    assert_equal "1 month ago", str
+  end
+
+  test "#relative_time works (> 1 week ago)" do
+    test_time = Time.parse('6th May 2018 3:00:00 PM')
+    str = test_time.strftime(relative_time(test_time))
+
+    assert_equal "9 days ago", str
+  end
+
+  test "#relative_time works (> 1 day ago)" do
+    test_time = Time.parse('10th May 2018 3:00:00 PM')
+    str = test_time.strftime(relative_time(test_time))
+
+    assert_equal "last Thursday at 3:00 PM", str
+  end
+
+  test "#relative_time works (yesterday)" do
+    test_time = Time.parse('14th May 2018 3:00:00 PM')
+    str = test_time.strftime(relative_time(test_time))
+
+    assert_equal "yesterday at 3:00 PM", str
+  end
+
+  test "#relative_time works (>= 1 hour ago)" do
+    test_time = Time.parse('15th May 2018 2:30:00 PM')
+    str = test_time.strftime(relative_time(test_time))
+
+    assert_equal "2:30 PM", str
+  end
+
+  test "#relative_time works (< 1 hour ago)" do
+    test_time = Time.parse('15th May 2018 4:45:00 PM')
+    str = test_time.strftime(relative_time(test_time))
+
+    assert_equal "15 minutes ago (4:45 PM)", str
+  end
+
+  test "#relative_time works (< 1 minute ago)" do
+    test_time = Time.parse('15th May 2018 4:59:30 PM')
+    str = test_time.strftime(relative_time(test_time))
+
+    assert_equal "30 seconds ago (4:59 PM)", str
+  end
+
+  # ----------------------------------
+  # #relative_time - Future Times
+  # ----------------------------------
+
+  test "#relative_time works (< 1 minute from now)" do
+    test_time = Time.parse('15th May 2018 5:00:30 PM')
+    str = test_time.strftime(relative_time(test_time))
+
+    assert_equal "30 seconds from now (5:00 PM)", str
+  end
+
+  test "#relative_time works (< 1 hour from now)" do
+    test_time = Time.parse('15th May 2018 5:30:00 PM')
+    str = test_time.strftime(relative_time(test_time))
+
+    assert_equal "30 minutes from now (5:30 PM)", str
+  end
+
+  test "#relative_time works (>= 1 hour from now)" do
+    test_time = Time.parse('15th May 2018 11:30:00 PM')
+    str = test_time.strftime(relative_time(test_time))
+
+    assert_equal "11:30 PM", str
+  end
+
+  test "#relative_time works (tomorrow)" do
+    test_time = Time.parse('16th May 2018 6:00:00 PM')
+    str = test_time.strftime(relative_time(test_time))
+
+    assert_equal "tomorrow at 6:00 PM", str
+  end
+
+  test "#relative_time works (> 1 day from now)" do
+    test_time = Time.parse('17th May 2018 7:00:00 PM')
+    str = test_time.strftime(relative_time(test_time))
+
+    assert_equal "Thursday at 7:00 PM", str
+  end
+
+  test "#relative_time works (> 1 week from now)" do
+    test_time = Time.parse('23rd May 2018 3:00:00 PM')
+    str = test_time.strftime(relative_time(test_time))
+
+    assert_equal "8 days from now", str
+  end
+
+  test "#relative_time works (> 1 month from now)" do
+    test_time = Time.parse('20th June 2018 5:30:00 PM')
+    str = test_time.strftime(relative_time(test_time))
+
+    assert_equal "1 month from now", str
+  end
+
+  # ----------------------------------
+  # other tests
+  # ----------------------------------
+
+  test "#range works" do
+    assert_equal [], range(0, 0, 1).to_a
+    assert_equal [2], range(2, 3, 1).to_a
+    assert_equal [0, 1, 2], range(0, 3, 1).to_a
+    assert_equal [2, 4, 6], range(2, 8, 2).to_a
+  end
+end
