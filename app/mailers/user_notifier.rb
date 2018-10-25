ffc442b194cadf1d12a77a824dd51c4dd182ae90
Viktor Köves <viktorkoves@gmail.com>
2018-10-24 23:08:17 -0500

    Updated event invite email title and improve docs

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,14 +1,17 @@
+# Note: To test email sending locally, do UserNotifier._method_(parameters).deliver
+# Ex: UserNotifier.send_signup_email(User.first).deliver
 class UserNotifier < ApplicationMailer
   default :from => 'Carpe <do-not-reply@carpe.us>'
 
-  # send a signup email to the user, pass in the user object that   contains the user's email address
+  # Send a signup email to the user, pass in the user object that contains the user's email address
   def send_signup_email(user)
     @user = user
     mail(:to => @user.email, :subject => 'Welcome to Carpe')
   end
 
+  # Send an email about an event invite
   def event_invite_email(user)
     @user = user
-    mail(:to => @user.email, :subject => 'Welcome to Carpe')
+    mail(:to => @user.email, :subject => 'You Have Been Invited to an Event!')
   end
 end
