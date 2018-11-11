c0278b0eb83d075075b698cba144b259c1844d4f
Viktor Köves <viktorkoves@gmail.com>
2018-11-11 15:38:08 -0600

    Added event invite email routes Also added general event_invites_controller to handle this. These event invite updating routes are currently insecure, so they should be updated

---
+++ app/controllers/event_invites_controller.rb
@@ -0,0 +1,19 @@
+class EventInvitesController < ApplicationController
+  # Called when clicking the links in an event invite email
+  def email_action
+    # Do not allow setting status back to pending
+    if params[:new_status] != 'pending_response'
+      invite = EventInvite.find(params[:id])
+
+      if invite.update(status: params[:new_status])
+        flash[:notice] = "We have updated that event invite, you are now marked as \"#{params[:new_status]}\""
+      else
+        flash[:alert] = "Something went wrong responding to that event invite!"
+      end
+    else
+      flash[:alert] = "You cannot mark yourself as pending response on an event invite!"
+    end
+
+    redirect_to home_path
+  end
+end
