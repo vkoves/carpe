3641e1def594f1a9292cbf3e4a8826a56c0a3943
Viktor Köves <3187531+vkoves@users.noreply.github.com>
2019-02-17 15:09:57 -0600

    Made categories display crudely on scheduler beta

--- app/assets/javascripts/schedule/scheduler-beta.js
+++ app/assets/javascripts/schedule/scheduler-beta.js
@@ -1,9 +1,7 @@
 window.onload = function () {
     var app = new Vue({
-        el: '#app',
-        data: {
-            message: 'Hello Vue!'
-        }
+        el: '#vue-app',
+        data: { }
        }
     );
 };
