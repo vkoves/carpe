20f268a76e98d500930fe7d2dc9045d8d2d0fce2
Viktor Köves <3187531+vkoves@users.noreply.github.com>
2019-02-21 20:53:42 -0600

    Fixed bad CSP on prod

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -28,7 +28,5 @@
 Rails.application.config.content_security_policy do |policy|
   if Rails.env.development?
     policy.script_src :self, :https, :unsafe_eval, :unsafe_inline
-  else
-    policy.script_src :self, :https, :inline
   end
 end
