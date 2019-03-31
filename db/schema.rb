1e0c55e128a765ec1f44fe03a572e02d3053c45e
Viktor Köves <3187531+vkoves@users.noreply.github.com>
2019-03-31 17:03:28 -0500

    Added email confirmation via Devise confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_24_064632) do
+ActiveRecord::Schema.define(version: 2019_03_31_214009) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -164,6 +164,10 @@ ActiveRecord::Schema.define(version: 2018_11_24_064632) do
     t.datetime "banner_updated_at"
     t.string "custom_url", default: ""
     t.integer "default_event_invite_category_id"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["default_event_invite_category_id"], name: "index_users_on_default_event_invite_category_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
