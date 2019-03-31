90b4df512065a876aefb9fc92b2db47d1b8e94ba
Viktor Köves <3187531+vkoves@users.noreply.github.com>
2019-03-31 17:05:01 -0500

    Made email confirmation required after 1 day A longer time does not help old users

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -107,9 +107,7 @@ Devise.setup do |config|
   # able to access the website for two days without confirming their account,
   # access will be blocked just in the third day. Default is 0.days, meaning
   # the user cannot access the website without confirming their account.
-  # TODO: Make this smaller (or 0) after we deploy - we want to give existing
-  # Carpe users time to confirm their email, but new users shouldn't get that
-  config.allow_unconfirmed_access_for = 7.days
+  # config.allow_unconfirmed_access_for = 7.days
 
   # A period that the user is allowed to confirm their account before their
   # token becomes invalid. For example, if set to 3.days, the user can confirm
