f30c6077fc101395118ec84d4e769548f7d2cdfd
Watercycle <Watercycle@email.com>
2019-04-19 22:15:40 -0500

    Reworded 'only' tests

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -29,7 +29,7 @@ class GroupTest < ActiveSupport::TestCase
     assert_not users(:viktors_friend).in_group?(@group1)
   end
 
-  test "only owner can promote users in group" do
+  test "group owners can promote group members" do
     ability = Ability.new(users(:joe))
     assert ability.can? :update, groups(:four)
   end
