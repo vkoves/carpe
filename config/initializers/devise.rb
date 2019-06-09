6dba6ba036ecda09358e626dfdb2eae87ce9f2d0
Viktor Köves <3187531+vkoves@users.noreply.github.com>
2019-06-09 12:24:12 -0500

    Removed Google OAuth APP_ID and APP_SECRET This will also be updated, so good luck trying to use it ;)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -244,7 +244,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
   require "omniauth-google-oauth2"
-  config.omniauth :google_oauth2, "691236853053-d74vecl5lkc73seoi1qhgta87irllcak.apps.googleusercontent.com", "eK9EIkCjW0Gw5RLX8vbwgp0F", access_type: "offline", approval_prompt: ""
+  config.omniauth :google_oauth2, ENV["GOOGLE_OAUTH_APP_ID"], ENV["GOOGLE_OAUTH_APP_SECRET"], access_type: "offline", approval_prompt: ""
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
